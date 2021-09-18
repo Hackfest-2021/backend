2d7301fcbabfb08093c1a14d4615a5a382d33acf
shakeeb <shakeeb@retrocausal.ai>
2021-09-18 11:33:35 +0500

    Added alert types api

--- alerts/urls.py
+++ alerts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from alerts.views import AlertCreateList
+from alerts.views import AlertCreateList, alert_types
 
 app_name = 'alerts'
 urlpatterns = [
     path('', AlertCreateList.as_view(), name="create_alert"),
+    path('types/', alert_types, name="create_alert"),
 
 ]
--- alerts/views.py
+++ alerts/views.py
@@ -4,12 +4,20 @@ from rest_framework import status, generics, permissions, mixins
 
 # Create your views here.
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
-from alerts.models import Alerts
-from alerts.serializers import AlertSerializer
+from alerts.models import Alerts, AlertType
+from alerts.serializers import AlertSerializer, AlertTypeSerializer
+
 
 class AlertCreateList(generics.ListCreateAPIView):
     # permission_classes = (CsrfExemptSessionAuthentication,AllowAny,)
     queryset = Alerts.objects.all()
     serializer_class = AlertSerializer
+
+@api_view(["GET"])
+def alert_types(request):
+
+    return Response({"alert_types": AlertTypeSerializer(AlertType.objects.all(),many=True).data})
